d4165cbfcfd5bc398bcc21401966a6c0dd17738a
fanifrancs <fanikufran6@gmail.com>
2022-08-05 01:41:46 +0100

    second commit

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
 let inputField = document.querySelector('input');
 let searchBtn = document.querySelector('button');
 let container = document.querySelector('div.container');
-let response;
 
 searchBtn.addEventListener('click', getMovies);
 
-function getMovies(e) {
-    e.preventDefault();
+function getMovies(event) {
+    event.preventDefault();
     container.innerHTML = '';
     let query= inputField.value;
     let xhr = new XMLHttpRequest();
     let url = 'https://www.omdbapi.com/?apikey=1691f1b1&s=' + query;
-    xhr.open('GET', url, true);
+    xhr.open('GET', url);
     xhr.onload = function() {
         if (this.status === 200) {
-            response = JSON.parse(this.responseText);
-            renderMovies();
+            renderMovies(JSON.parse(this.responseText));
+        } else {
+            alert('Something went wrong');
         }
+    }
+    xhr.onerror = function() {
+        alert('Something went wrong');
     }
     xhr.send();
 }
 
-function renderMovies() {
-    response['Search'].forEach((movie) => {
+function renderMovies(param) {
+    param['Search'].forEach((movie) => {
         container.innerHTML += `
         <div class="gallery">
             <img src="${movie.Poster}" alt="${movie.Title}">
